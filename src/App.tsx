02bdfae05a7016dabc58740ba445e2b08f1c99db
Pitt Wu <kgb00128@gmail.com>
2022-01-06 14:36:13 +0800

    feat: install & setting eslint

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import 'App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello React with TypeScript</h1>
+      <h1>
+        Hello React with TypeScript
+      </h1>
     </div>
   );
 }
