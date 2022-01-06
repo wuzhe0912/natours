53b5d7d15c550193f89aa55194f210b15328a891
Pitt Wu <kgb00128@gmail.com>
2022-01-06 15:46:43 +0800

    feat: install & setting prettier

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import 'App.css';
 function App() {
   return (
     <div className="App">
-      <h1>
-        Hello React with TypeScript
-      </h1>
+      <h1>Hello React with TypeScript</h1>
     </div>
   );
 }
